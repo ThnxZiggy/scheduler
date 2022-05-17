3489ef6d5d9751eef7603ebf975a5ba58de1ae7c
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-17 14:18:14 -0400

    adding button functionality with storybook

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,20 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
-}
+   const {confirm, danger, onClick, disabled} = props
+   let buttonClass = "button";
+   if (confirm) {
+     buttonClass += " button--confirm";
+   }
+
+   if (danger) {
+      buttonClass += " button--danger";
+    }
+   
+   if (onClick) {
+      
+   }
+  
+ 
+   return <button onclick={onClick} disabled={disabled} className={buttonClass}>{props.children}</button>;
+ }
