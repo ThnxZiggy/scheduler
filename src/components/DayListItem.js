13c3a958996b19e0d9517bb29ea660718fa4b329
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-17 21:29:06 -0400

    add spot conditional

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,17 +2,26 @@ import React from "react";
 import "components/DayListItem.scss";
 import classNames from "classnames";
 
+const formatSpots = function(spots) {
+  if(spots === 1) {
+    return spots + ` spot`  
+  }  
+  return `${spots === 0 ? 'no' : spots} spots`      
+};
+
 export default function DayListItem(props) {
   const {name, spots, selected, setDay} = props;
   const dayClass = classNames('day-list__item', {
     "day-list__item--selected": selected,
-    "day-list__item--full": spots === 0
+    "day-list__item--full": (spots === 0)
   })
+  
+  const clickHandler = () => setDay(name)
 
   return (
-    <li className={dayClass} onClick={() => setDay(name)}>
+    <li className={dayClass} onClick={clickHandler}>
       <h2 className="text--regular">{name}</h2> 
-      <h3 selected={selected} className="text--light">{spots} Spots remaining</h3>
+      <h3 selected={selected} className="text--light">{formatSpots(spots)} remaining</h3>
     </li>
   );
 }
