115b5e54d5989ca7cc7c72180be9242e65ca7727
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-05-21 20:35:13 -0400

    add interviewers helper function and validate tests

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from "axios";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 // const appointments = {
 //   1: {
@@ -62,25 +62,36 @@ export default function Application(props) {
     const appointmentsURL = `http://localhost:8001/api/appointments`;
     const interviewersURL = `http://localhost:8001/api/interviewers`;
     Promise.all([
-    axios.get(daysURL),
-    axios.get(appointmentsURL),
-    axios.get(interviewersURL)])
-    .then((all) => {
-      setState((prev) => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
+      axios.get(daysURL),
+      axios.get(appointmentsURL),
+      axios.get(interviewersURL),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
       // console.log(JSON.stringify(response))
       // setDays([...response.data]);
       console.log(all);
-    })
-  }, []); 
+    });
+  }, []);
 
   // const dailyAppointments = [];
-  const dailyAppointments = getAppointmentsForDay(state, state.day)
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const schedule = dailyAppointments.map((appointment) => {
-    return <Appointment key={appointment.id} {...appointment} />;
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
   });
-
-  
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,21 +1,21 @@
 export function getAppointmentsForDay(state, day) {
-
   // find the day
-  const filteredNames = state.days.find(item => item.name === day);
+  const filteredNames = state.days.find((item) => item.name === day);
   if (!filteredNames) {
-    return []
+    return [];
   }
 
   //find appointment for the given day
-  const arr = filteredNames.appointments
-  const appointmentsObjectToArray = Object.values(state.appointments);
-  const appointments = appointmentsObjectToArray.filter(item => arr.includes(item.id))
-  
-  return appointments;
+  const arr = filteredNames.appointments;
+  const mappedArr = arr.map((id) => {
+    return state.appointments[id];
+  });
 
-}
+  return mappedArr;
+};
+
 // dayListItems = days.map((singleDay) => (
-  // <DayListItem
+// <DayListItem
 
 //   // find the day
 //   const filteredNames = state.days.filter(item => item.name === day);
@@ -27,7 +27,28 @@ export function getAppointmentsForDay(state, day) {
 //   const arr = filteredNames[0].appointments
 //   const appointmentsObjectToArray = Object.values(state.appointments);
 //   const appointments = appointmentsObjectToArray.filter(item => arr.includes(item.id))
-  
-//   return appointments;
 
-// }
+//version 2 of arr
+// const arr = filteredNames.appointments;
+// const appointmentsObjectToArray = Object.values(state.appointments);
+// const appointments = appointmentsObjectToArray.filter((item) =>
+//   arr.includes(item.id)
+// );
+// return appointments;
+
+export function getInterview(state, interview) {
+  //go through appointments object
+  //where state.appointments.interview = true/not null
+  //grab state.appointments.interview.interviewer value
+  //connect with state.interviewers and return value where state.appointments.interview.interviewer === state.interviewers.id
+
+  if (!interview) {
+    return null;
+  }
+  const id = interview.interviewer;
+  const updatedInterview = {
+    ...interview,
+    interviewer: state.interviewers[id],
+  };
+  return updatedInterview;
+};
